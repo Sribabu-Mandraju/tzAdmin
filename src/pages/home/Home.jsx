38af82449a8e46b6fd1d859d8da35252b2d7bc02
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-01 11:25:51 +0530

    dashboard,notification

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,50 +4,50 @@ const Home = () => {
   return (
     <Layout>
       <div className="dashboard ">
-        <div className="registrations-data grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
-          <div className="registration-statistics grid grid-cols-2 grid-rows-2 gap-4 md:w-[400px] h-[250px]">
-          <div className="total-registrations col-span-1 p-4 text-black rounded-lg shadow-lg">
-            <h2 className="text-lg font-semibold">Total Registrations</h2> {/* Corrected font size */}
-            <h2 className="registrations-count text-xl font-semibold">0</h2>
+        <div className="registrations-data grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 w-full">
+          <div className="registration-statistics grid grid-cols-2 grid-rows-2 gap-4 md:w-[500px] h-[250px]">
+          <div className="total-registrations border border-zinc-300 col-span-1 p-4 text-black rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold">Total <br className="sm:hidden"></br> Registrations</h2> {/* Corrected font size */}
+            <h2 className="registrations-count text-3xl font-semibold">0</h2>
           </div>
-          <div className="fromcampus p-4 text-black rounded-lg shadow-lg">
-            <h2 className="text-lg font-semibold">From Rgukt</h2>
-            <h2 className="registrations-count text-lg font-semibold">0</h2>
+          <div className="fromcampus p-4 border border-zinc-300 text-black rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold ">From <br className="sm:hidden"></br> Rgukt</h2>
+            <h2 className="registrations-count text-3xl font-semibold">0</h2>
           </div>
-          <div className="offcampus p-4 text-black rounded-lg shadow-lg">
-            <h2 className="text-lg font-semibold">Off Campus</h2>
-            <h2 className="registrations-count text-lg font-semibold">0</h2>
+          <div className="offcampus p-4 border border-zinc-300 text-black rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold">Off <br className="sm:hidden"></br>Campus</h2>
+            <h2 className="registrations-count text-3xl font-semibold">0</h2>
           </div>
-          <div className="received-money p-4 text-black rounded-lg shadow-lg">
-            <h2 className="text-lg font-semibold">Received Money</h2>
-            <h2 className="total-money text-lg font-semibold">0</h2>
+          <div className="received-money border border-zinc-300 p-4 text-black rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold">Received <br className="sm:hidden"></br> Money</h2>
+            <h2 className="total-money text-3xl font-semibold ">0</h2>
           </div>
         </div>
         <div className="registrations-graph">
         </div>
         </div>
         <div className="department-registrations grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4 py-[30px]">
-          <div className="fromcse shadow-lg text-lg rounded-lg font-semibold p-[15px] ">
+          <div className="fromcse border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px] ">
             <h1 className="font-bold">CSE</h1>
             <h2 className="text-[18px]">Total registrations: <span className='count-cse'>0</span></h2>
           </div>
-          <div className="fromece shadow-lg text-lg rounded-lg font-semibold p-[15px]">
+          <div className="fromece border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px]">
             <h1 className="font-bold">ECE</h1>
             <h2>Total registrations: <span className='count-ece'>0</span></h2>
           </div>
-          <div className="frommechanical shadow-lg text-lg rounded-lg font-semibold p-[15px]">
+          <div className="frommechanical border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px]">
             <h1 className="font-bold">MECHANICAL</h1>
             <h2>Total registrations: <span className='count-mechanical'>0</span></h2>
           </div>
-          <div className="fromcivil shadow-lg text-lg rounded-lg font-semibold p-[15px]">
+          <div className="fromcivil border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px]">
             <h1 className="font-bold">CIVIL</h1>
             <h2>Total registrations: <span className='count-civil'>0</span></h2>
           </div>
-          <div className="fromchemical shadow-lg text-lg rounded-lg font-semibold p-[15px]">
+          <div className="fromchemical border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px]">
             <h1 className="font-bold">CHEMICAL</h1>
             <h2>Total registrations: <span className='count-chemical'>0</span></h2>
           </div>
-          <div className="frommetallurgy shadow-lg text-lg rounded-lg font-semibold p-[15px]">
+          <div className="frommetallurgy border border-zinc-300 shadow-lg text-lg rounded-lg font-semibold p-[15px]">
             <h1 className="font-bold">METALLURGY</h1>
             <h2>Total registrations: <span className='count-metallurgy'>0</span></h2>
           </div>
--- src/pages/notifications/CreateNotification.jsx
+++ src/pages/notifications/CreateNotification.jsx
@@ -5,11 +5,11 @@ const CreateNotification = () => {
   return (
     <Layout>
       <div className="flex justify-center">
-        <form className='flex flex-col gap-2 rounded-lg w-full'>
-        <h1 className='font-semibold'>NOTIFICATION</h1>
+        <form className='flex flex-col gap-2 rounded-lg w-[500px] shadow-lg p-[50px] border-[1px]'>
         <textarea className='border-2 outline-[#ccc] rounded-md p-[10px]' placeholder='Notification Description'></textarea>
         <input type="text" placeholder='Picturepath' className='border-2 p-[10px] outline-[#ccc] rounded-lg' />
         <input type="text" placeholder='important link' className='border-2 p-[10px] outline-[#ccc] rounded-lg' />
+        <input type="submit" className='bg-black text-white h-[50px] rounded-lg' />
         </form>
         
       </div>
