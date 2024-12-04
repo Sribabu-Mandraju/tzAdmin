f6c69ce6b4643722b7045ed660087ae6515cf24f
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-04 15:15:59 +0530

    notifications

--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -8,9 +8,9 @@ const Notifications = () => {
   
 
   const data=[
-    {id:1,name:"hi",status:"succesful",type:"assigned",email:"gmail.com",sign:"1 year ago",userId:"890"},
-    {id:2,name:"hello",status:"succesful",type:"assigned",email:"gmail.com",sign:"1 year ago",userId:"89"},
-    {id:3,name:"hey",status:"succesful",type:"assigned",email:"gmail.com",sign:"1 year ago",userId:"8"},
+    {id:1,name:"hi",status:"succesful",type:"assigned",email:"abc@gmail.com",sign:"1 year ago",userId:"890"},
+    {id:2,name:"hello",status:"succesful",type:"assigned",email:"abc@gmail.com",sign:"1 year ago",userId:"89"},
+    {id:3,name:"hey",status:"succesful",type:"assigned",email:"abc@gmail.com",sign:"1 year ago",userId:"8"},
   ]
 
 
@@ -19,8 +19,8 @@ const Notifications = () => {
       <div className="w-full  flex items-center justify-end">
         <button className="bg-black text-white px-3 py-2 rounded-md font-semibold" onClick={() => navigate("/notifications/create")}>Add +</button>
       </div>
-      <div className='w-full overflow-x-scroll'>
-      <table className="border w-full min-w-[1000px] mt-4  sm:w-full  text-left text-nowrap">
+      <div className='w-full overflow-x-scroll lg:overflow-x-hidden rounded-md'>
+      <table className="border w-full mt-6  sm:w-full  text-left text-nowrap rounded-md">
         <thead className="bg-gray-200">
             <th className="p-3 ">FULL NAME</th>
             <th className="p-3">STATUS</th>
@@ -31,7 +31,7 @@ const Notifications = () => {
         </thead>
         <tbody>
           {data.map((item,index)=>(
-            <tr key={index}>
+            <tr key={index} className='even:bg-[aliceblue] odd:bg-[#ccc]'>
               <td className="p-3">{item.name}</td>
               <td className="p-3">{item.status}</td>
               <td className="p-3">{item.type}</td>
