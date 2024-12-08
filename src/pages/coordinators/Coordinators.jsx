6f27c93aac7a7f6636e7b7ea269fd06e0b7ea680
sribabu <sribabumandraju@gmail.com>
2024-12-08 10:49:05 +0530

    s

--- src/pages/coordinators/Coordinators.jsx
+++ src/pages/coordinators/Coordinators.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from '../../components/layouts/Layout';
 import CoordinatorsCard from "./card";
-import TZ from '../../assets/TZLOGO.svg'
+import TZ from '../../assets/fullstack.png'
 const Coordinators = () => {
   const navigate = useNavigate()
   return (
