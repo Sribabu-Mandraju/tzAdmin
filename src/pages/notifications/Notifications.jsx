7d3baf4c7e1696f1ba7b9898197221c7a10719b1
sribabu <sribabumandraju@gmail.com>
2025-01-31 23:29:13 +0530

    new

--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -17,7 +17,7 @@ const Notifications = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [category, setCategory] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const rowsPerPage = 2;
+  const rowsPerPage = 10;
 
   // Handle Search
   const handleSearch = (event) => {
@@ -97,24 +97,25 @@ const Notifications = () => {
           <thead className="bg-black text-white">
             <tr>
               <th className="p-3 text-nowrap">Id</th>
-              <th className="p-3 text-nowrap">Full Name</th>
-              <th className="p-3 text-nowrap">Status</th>
-              <th className="p-3 text-nowrap">Type</th>
-              <th className="p-3 text-nowrap">Email</th>
-              <th className="p-3 text-nowrap">Signed Up</th>
-              <th className="p-3 text-nowrap">User Id</th>
+              <th className="p-3 text-nowrap">Title</th>
+              <th className="p-3 text-nowrap">Info</th>
+              <th className="p-3 text-nowrap">Time</th>
+              <th className="p-3 text-nowrap">Actions</th>
             </tr>
           </thead>
           <tbody>
             {paginatedData.map((item, index) => (
               <tr key={index} className="border">
                 <td className="p-3">{index + 1}</td>
-                <td className="p-3">{item.name}</td>
-                <td className="p-3">{item.status}</td>
-                <td className="p-3">{item.type}</td>
-                <td className="p-3">{item.email}</td>
-                <td className="p-3">{item.sign}</td>
-                <td className="p-3">{item.userId}</td>
+                <td className="p-3">item.heading</td>
+                <td className="p-3">
+                  {" "}
+                  <div dangerouslySetInnerHTML={{ __html: item.info.slice(0,9) }} />
+                </td>
+                <th className="p-3 text-nowrap">{new Date(item.createdAt).toLocaleString()}</th>
+
+                <th className="p-3 text-nowrap">Actions</th>
+
               </tr>
             ))}
           </tbody>
